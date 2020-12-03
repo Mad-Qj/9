36d6513ef994ae00fb786020cecad60c1f1746de
Mad-Qj <72621279+Mad-Qj@users.noreply.github.com>
2020-12-03 13:54:38 +0400

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc8b5a098cea1001eadaab2",
+				"5fc8b5a098cea1001eadaab4"
+			]
+		},
+		"5fc8b5a098cea1001eadaab2": {
+			"id": "5fc8b5a098cea1001eadaab2",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc8b5a098cea1001eadaab4": {
+			"id": "5fc8b5a098cea1001eadaab4",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
